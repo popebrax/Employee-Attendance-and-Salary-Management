06dedb48e1d6797a7c5bbb2b1609f9c5a02576e3
popebrax <147528968+popebrax@users.noreply.github.com>
2024-04-15 13:00:17 +0300

    Update Employeeperformance.js

--- app/(home)/Employeeperformance.js
+++ app/(home)/Employeeperformance.js
@@ -30,7 +30,7 @@ const fetchAttendanceReport = async () => {
     const currentMonth = currentDate.month() + 1; // Month is zero-based, so we add 1
     const currentYear = currentDate.year();
 
-    const response = await axios.get('http://192.168.43.99:8000/attendance-report-all-employees', {
+    const response = await axios.get('http://localhost:8000/attendance-report-all-employees', {
       params: {
         month: currentMonth,
         year: currentYear,
